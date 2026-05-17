e15c67fda566b93b9f6518aa28df37f165570bbd
Gustavo de Oliveira <gustavogdeoliveira27@gmail.com>
2026-05-17 00:12:04 -0300

    Exercícios de revisão de condicionais  simples

---
+++ simples/Exer.20.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um número: "))
+
+if 10 < num < 20:
+    print(f"O número {num} está entre 10 e 20!")
+else:
+    print(f"O número {num} não está entre 10 e 20!")
---
+++ simples/Exer.21.py
@@ -0,0 +1,6 @@
+num = int(input("Digite um número: "))
+
+if num % 2 == 0:
+    print("Seu número é Par!")
+else:
+    print("Seu número é Ímpar!")
